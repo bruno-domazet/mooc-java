15a3dbb075ff123368366c87b5c82cb85436180c
bosdo <bosdo@jfmedier.dk>
2022-02-25 11:35:54 +0100

    gradle hello-world

---
+++ src/main/java/hello.java
@@ -0,0 +1,11 @@
+package src.main.java;
+
+/**
+ * hello
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("args are:");
+        System.out.println(args);
+    }
+}
